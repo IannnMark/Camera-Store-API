3e85fd367a52aec88df60f08755572f7991b739d
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-15 20:34:06 +0800

    create user sign in

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,3 +14,24 @@ exports.signUp = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        //destructuring ng password para hindi makita kapag tinest sa backend
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
