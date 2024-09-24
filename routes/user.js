842eb4d2a73b15a8d76f7701b8bd1d1603571136
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-24 09:56:58 +0800

    delete user route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { updateUser } = require("../controllers/userController");
+const { updateUser, deleteUser } = require("../controllers/userController");
 const verifyToken = require("../utils/verifyUser");
 const { route } = require("./auth");
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
+router.delete("delete/:id", verifyToken, deleteUser);
 
 module.exports = router;
