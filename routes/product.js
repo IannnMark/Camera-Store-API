dd9e46a61a42857575d8acb7b0ee01e4b154b6e9
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-25 11:37:55 +0800

    get all admin products route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { createProduct, deleteProduct, updateProduct } = require("../controllers/productController");
+const { createProduct, deleteProduct, updateProduct, getAdminProducts } = require("../controllers/productController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 const router = express.Router();
 
 router.post("/admin/product/new", verifyToken, authorizeRoles('admin'), createProduct);
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct);
 router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
+router.get("/admin/products", verifyToken, authorizeRoles("admin"), getAdminProducts);
 
 module.exports = router;
