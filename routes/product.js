d8d41517936db279d2ea00c8ccb2e387a411eb18
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-25 10:16:38 +0800

    delete product route

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { createProduct } = require("../controllers/productController");
+const { createProduct, deleteProduct } = require("../controllers/productController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 const router = express.Router();
 
 router.post("/admin/product/new", verifyToken, authorizeRoles('admin'), createProduct);
+router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;
