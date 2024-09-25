c9d78f34176a7b8eba75525e98723a2c49b207f0
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-25 09:42:21 +0800

    add role to the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,11 +20,12 @@ exports.signIn = async (req, res, next) => {
     const { email, password } = req.body;
 
     try {
+        const user = await User.findOne({ email: req.body.email });
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, "User not found"));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: validUser._id, role: user.role }, process.env.JWT_SECRET)
 
         //destructuring ng password para hindi makita kapag tinest sa backend
         const { password: pass, ...rest } = validUser._doc;
@@ -50,7 +51,7 @@ exports.google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email: req.body.email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET)
             const { password: pass, ...rest } = user._doc;
             res
                 .cookie('access_token', token, { httpOnly: true })
