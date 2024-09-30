49cb7bd5ed7e83eb681e0b0683f9a0f8dcd224f5
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-30 19:07:06 +0800

    add admin delete route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { updateUser, deleteUser, allUsers } = require("../controllers/userController");
+const { updateUser, deleteUser, allUsers, eraseUser } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/admin/users", verifyToken, authorizeRoles("admin"), allUsers)
-
+router.get("/admin/users", verifyToken, authorizeRoles("admin"), allUsers);
+router.delete("/admin/delete/:id", verifyToken, authorizeRoles("admin"), eraseUser);
 
 module.exports = router;
