86628b0d6a50132b6a08123dda058adadaf3aa7c
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-04 14:11:37 +0800

    create new order controller

---
+++ controllers/orderController.js
@@ -0,0 +1,33 @@
+const Order = require("../models/order");
+const Product = require("../models/product");
+const User = require("../models/user");
+
+
+exports.newOrder = async (req, res, next) => {
+    const { orderItems, paymentInfo, itemsPrice, totalPrice, screenshot, referenceNumber } = req.body;
+
+    if (!orderItems || !paymentInfo || !itemsPrice || !totalPrice) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide all required fields",
+        });
+    }
+
+    try {
+        const order = await Order.create({
+            orderItems,
+            paymentInfo,
+            itemsPrice,
+            totalPrice,
+            screenshot,
+            referenceNumber,
+            user: req.user._id,
+        })
+        res.status(200).json({
+            success: true,
+            order,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
