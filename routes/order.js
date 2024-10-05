a226f53717a9d43c0c9ee697522f1452088d9b65
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-05 19:07:44 +0800

    add get single order route

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const { newOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder } = require("../controllers/orderController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
 
 
 router.post("/order/new", verifyToken, newOrder);
+router.get("/order/:id", verifyToken, getSingleOrder);
 
 
 
