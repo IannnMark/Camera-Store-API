9d00455206ebaee052f4e27ec2a6bcd74a173e8c
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-05 19:07:25 +0800

    create get single order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order = require("../models/order");
 const Product = require("../models/product");
 const User = require("../models/user");
+const errorHandler = require("../utils/error");
 
 
 exports.newOrder = async (req, res, next) => {
@@ -26,6 +27,23 @@ exports.newOrder = async (req, res, next) => {
         })
         res.status(200).json({
             success: true,
+            order,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.getSingleOrder = async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', ["username", "paymentInfo"]);
+
+    try {
+        if (!order) {
+            return next(errorHandler(404, "No Order found with this ID"));
+        }
+        res.status(200).json({
+            success: true,
+
             order,
         });
     } catch (error) {
