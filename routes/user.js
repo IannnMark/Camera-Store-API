6e92d6dc4c641fb659f0284bdaa813a4bb10d52f
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-14 11:09:40 +0800

    soft-delete user route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { updateUser, deleteUser, allUsers, eraseUser } = require("../controllers/userController");
+const { updateUser, deleteUser, allUsers, eraseUser, softDeleteUser } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/admin/users", verifyToken, authorizeRoles("admin"), allUsers);
 router.delete("/admin/delete/:id", verifyToken, authorizeRoles("admin"), eraseUser);
+router.delete("/admin/user/soft-delete/:id", verifyToken, authorizeRoles("admin"), softDeleteUser);
 
 module.exports = router;
